acea587d9e6acfe3f2c138482aad26268a68ea0c
Anne <rendino57@gmail.com>
2019-11-23 22:53:35 -0600

    Solution

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,14 +2,9 @@
 
 import csv
 
-
-
-
-
 with open("budget_data.csv") as budget:
     csv_reader = csv.reader(budget)
     header = next(csv_reader)
-    print(header)
     months_of_data = 0
     net_total = 0
     last_profit = 0
@@ -25,17 +20,29 @@ with open("budget_data.csv") as budget:
         months_of_data += 1
         profit = int(row[1])
         net_total += profit
+        prof_chng = profit - last_profit
+        profit_changes += prof_chng
         if profit > last_profit:
-            if (profit - last_profit) > gpi:
-                gpi = profit - last_profit
+            if prof_chng > gpi:
+                greatest_profit_month = row[0]
+                gpi = prof_chng
         else:
-            if (profit - last_profit) < gpd:
-                gpd = profit - last_profit
+            if prof_chng < gpd:
+                greatest_loss_month = row[0]
+                gpd = prof_chng
 
         last_profit = profit
+    avg_profit_change = ('%.2f'%(profit_changes/months_of_data))
 
-print("Greatest profit increase was: ",  gpi, ". Greatest profit decrease was: ",  gpd)
+budget_report = open("budget_report.txt", "w+")
+print("     Budget Report \n ____________________")
+print("The net profit over this {0} month time frame was: ${1}.".format(months_of_data, net_total))
+print("The greatest profit increase was: $ {0} in {1}. The greatest profit decrease was: $ {2} in {3}.".format(gpi, greatest_profit_month, gpd, greatest_loss_month))
+print("The average change in profit was: ${0}.".format(avg_profit_change))
 
-#open("budget_report.txt", "w+")
-#budget_report.write(months_of_data)
-#budget_report.close()
+print("     Budget Report \n ____________________", file = budget_report)
+print("The net profit over this ", months_of_data, " month time frame was: $", net_total, ".", file = budget_report)
+print("Greatest profit increase was: $",  gpi, ". Greatest profit decrease was: $",  gpd, file = budget_report)
+print("The average change in profit was: $", avg_profit_change, ".", file = budget_report)
+
+budget_report.close()
